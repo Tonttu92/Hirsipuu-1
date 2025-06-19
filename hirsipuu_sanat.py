d67b061c7f16ec87ca695057fb795ccba0a9cfc7
Toni Barck <toni_barck@hotmail.com>
2025-06-19 23:22:40 +0300

    hirsipuu_sanat.py

--- hirsipuu_sanat.py
+++ hirsipuu_sanat.py
@@ -1,4 +1,4 @@
-import random
+from random import choice
 
 def lue_sanat_tiedostosta(tiedoston_nimi="sanat.txt"):
     try:
@@ -13,3 +13,41 @@ def valitse_sana(sanalista):
     if not sanalista:
         return None
     return random.choice(sanalista)
+
+class Pelinlogiikka:
+    def __init__(self, sana):
+        self.sana = sana
+        self.arvaukset = set()
+        self.yritykset = Sanatiedosto.arvausten_maara(self)
+
+    def arvaa(self, arvaus):
+        arvaus = arvaus.lower()
+        if not arvaus.isalpha():
+            return f"Anna vain kirjaimia."
+        
+        if len(arvaus) == 1:
+            if arvaus in self.arvaukset:
+                return f"Olet jo arvannut tämän kirjaimen."
+            self.arvaukset.add(arvaus)
+            if arvaus in self.sana:
+                return f"Kirjain on sanassa."
+            else:
+                self.yritykset -=1
+                return f"Väärä kirjain"
+        else:
+            if arvaus == self.sana:
+                self.arvaukset.update(self.sana)
+                return f"Oikea sana"
+            else:
+                self.yritykset -= 1
+                return f"väärä sana!! Sana ei ollut \"{arvaus}\"."
+            
+    def tilanne(self):
+        return [kirjain if kirjain in self.arvaukset else "_" for kirjain in self.sana]
+    
+    def loppu(self):
+        if self.yritykset == 0:
+            return True, f"HÄVISIT! Oikea sana oli: {self.sana}"
+        if all(k in self.arvaukset for k in self.sana):
+            return True, f"Onneksi olkoon! Arvasit sanan oikein: {self.sana}"
+        return False, ""
